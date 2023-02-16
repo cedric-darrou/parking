afb47f209bf325e0a2981b95670dee1e407a1368
Formation <c.darrou@gmx.com>
2023-02-16 10:23:35 +0100

    Add methods

--- include/Parking.hpp
+++ include/Parking.hpp
@@ -18,7 +18,19 @@ public:
     };
     virtual ~Parking();
 
-    //List of class
+    //Getter
+    unsigned int get_id_parking();
+    std::string get_nom();
+    std::string get_localisation();
+    std::string get_nb_places_occupees();
+
+    //Setter
+    void set_nom(std::string new_name);
+    void set_localisation(std::string new_adresse);
+
+    //Methods
+    void voiture_entre();
+    void voiture_sort();
 };
 
 #endif
